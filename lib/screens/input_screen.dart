a324f301ade6ed73cfa70fa46831acfd3593d813
zykuriku <104116273+zykuriku@users.noreply.github.com>
2024-05-04 23:29:13 +0530

    commit 2

--- lib/screens/input_screen.dart
+++ lib/screens/input_screen.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:dc/utilities/calculations.dart';
+import 'package:dc/screens/results_page.dart';
 
-class Input extends StatelessWidget {
+import '../utilities/calculations.dart';
+
+class Input extends StatefulWidget {
   const Input({super.key});
 
+  @override
+  State<Input> createState() => _InputState();
+}
+
+class _InputState extends State<Input> {
+  late double i1;
+  late double i2;
+  late double i3;
+  late double i4;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,41 +25,207 @@ class Input extends StatelessWidget {
             decoration:
                 BoxDecoration(borderRadius: BorderRadius.circular(20.0)),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    textAlign: TextAlign.center,
-                  ),
+                Text(
+                  'Hopkin\'s test',
+                  style: TextStyle(fontSize: 25.0),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    textAlign: TextAlign.center,
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Enter i1: ",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    SizedBox(
+                      width: 80.0,
+                      height: 25.0,
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        onChanged: (value) {
+                          i1 = value as double;
+                        },
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 20.0),
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.brown, width: 1.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.brown, width: 2.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    textAlign: TextAlign.center,
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Enter i2: ",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    SizedBox(
+                      width: 80.0,
+                      height: 25.0,
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        onChanged: (value) {
+                          i2 = value as double;
+                        },
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 20.0),
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.brown, width: 1.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.brown, width: 2.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    textAlign: TextAlign.center,
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Enter i3: ",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    SizedBox(
+                      height: 25.0,
+                      width: 80.0,
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        onChanged: (value) {
+                          i3 = value as double;
+                        },
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 20.0),
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.brown, width: 1.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.brown, width: 2.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Enter i4: ",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    SizedBox(
+                      height: 25.0,
+                      width: 80.0,
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        onChanged: (value) {
+                          i4 = value as double;
+                        },
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 20.0),
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.brown, width: 1.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.brown, width: 2.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
                 Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: FloatingActionButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    Calc(i1: i1, i2: i2, i3: i3, i4: i4)));
-                      },
+                    child: Material(
+                      color: Colors.redAccent,
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: MaterialButton(
+                          onPressed: () {
+                            Calc calculation =
+                                Calc(v: 230, i1: i1, i2: i2, i3: i3, i4: i4);
+                            calculation.calcMotor();
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Result()));
+                          },
+                          child: Text(
+                            'Generate values for DC motor',
+                            style: TextStyle(fontSize: 18.0),
+                          )),
+                    )),
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Material(
+                      color: Colors.redAccent,
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: MaterialButton(
+                          onPressed: () {
+                            Calc calculation =
+                                Calc(v: 230, i1: i1, i2: i2, i3: i3, i4: i4);
+                            calculation.calcGenerator();
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Result()));
+                          },
+                          child: Text(
+                            'Generate values for DC generator',
+                            style: TextStyle(fontSize: 18.0),
+                          )),
                     )),
               ],
             ),
